6a09bd2d5d728f548d6fe67a516edf101e17cb49
Ariel Nunez <ingenieroariel@gmail.com>
2011-06-30 17:11:22 -0400

    staticfiles fixes

--- src/GeoNodePy/geonode/settings.py
+++ src/GeoNodePy/geonode/settings.py
@@ -229,7 +229,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'django.contrib.admin.staticfiles',
+    'django.contrib.staticfiles',
     'django_extensions',
     'registration',
     'profiles',
--- src/GeoNodePy/geonode/urls.py
+++ src/GeoNodePy/geonode/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
