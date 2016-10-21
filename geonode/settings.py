aa853b012a13594a640921cdce7098f8332df161
Way Barrios <waybarrios@gmail.com>
2016-10-21 09:12:00 -0500

    Chaging geoserver public and internal location

--- geonode/settings.py
+++ geonode/settings.py
@@ -504,8 +504,9 @@ MISSING_THUMBNAIL = os.getenv('MISSING_THUMBNAIL','geonode/img/missing_thumb.png
 # Search Snippet Cache Time in Seconds
 CACHE_TIME = int(os.getenv('CACHE_TIME','0'))
 
-GEOSERVER_LOCATION = os.getenv('GEOSERVER_LOCATION', 'http://localhost:8080/geoserver/')
-GEOSERVER_PUBLIC_LOCATION = os.getenv('GEOSERVER_PUBLIC_LOCATION', 'http://localhost:8080/geoserver/')
+
+GEOSERVER_LOCATION= os.getenv('GEOSERVER_BASE_URL', 'http://geoserver:8080/geoserver/')
+GEOSERVER_PUBLIC_LOCATION = os.getenv ('GEOSERVER_PUBLIC_URL','http://localhost/geoserver/')
 
 # OGC (WMS/WFS/WCS) Server Settings
 # OGC (WMS/WFS/WCS) Server Settings
